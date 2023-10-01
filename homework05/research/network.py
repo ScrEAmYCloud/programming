5abde6988e09d2fa92e03ad6bba1823117dff0a3
ScrEAmYCloud <bigachev98@gmail.com>
2023-10-01 17:00:08 +0300

    Реализована функция ego_network.

--- homework05/research/network.py
+++ homework05/research/network.py
@@ -18,7 +18,13 @@ def ego_network(
     :param user_id: Идентификатор пользователя, для которого строится граф друзей.
     :param friends: Идентификаторы друзей, между которыми устанавливаются связи.
     """
-    pass
+    network = []
+    friends = friends or get_friends(user_id).items
+    mutual_friends = get_mutual(source_uid=user_id, target_uids=friends)
+    for target in mutual_friends:
+        for friend in target["common_friends"]:  
+            network.append((target["id"], friend))  
+    return network
 
 
 def plot_ego_network(net: tp.List[tp.Tuple[int, int]]) -> None:
